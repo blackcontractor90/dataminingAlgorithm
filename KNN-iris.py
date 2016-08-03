7b01a361f86a58db4d9885d9206ebf1970408cff
blackcontractor90 <farid_m2mfan@yahoo.com.my>
2016-08-03 17:01:19 +0800

    Create KNN-iris.py

---
+++ KNN-iris.py
@@ -0,0 +1,56 @@
+#test KNN algorithm with iris dataset
+import csv
+import random
+import math
+import operator
+
+	
+	#for row in lines:
+		#print ', '.join(row)
+
+def loadDataset(filename, split, trainingSet=[], testSet=[]):
+	with open('iris.data', 'rb') as csvfile:
+		lines = csv.reader(csvfile)
+		dataset = list(lines)
+		for x in range(len(dataset)-1):
+			for y in range(4):
+				dataset[x][y] = float(dataset[x][y])
+				if random.random() < split:
+					trainingSet.append(dataset[x])
+				else:
+					testSet.append(dataset[x])
+					#to test function with iris dataset
+					trainingSet=[]
+					testSet=[]
+					loadDataset('iris.data', 0.66, trainingSet, testSet)
+					print 'Train: ' + repr(len(trainingSet))
+					print 'Test:  ' + repr(len(testSet))
+
+def euclideanDistance(instance1, instance2, length):
+	distanc = 0
+	for x in range(length):
+		distance += pow((instance1[x] - instance2[x]), 2)
+		return math.sqrt(distance)
+		data1 = [2, 2, 2, 'a']
+		data2 = [4, 4, 4, 'b']
+		distance = euclideanDistance(data1, data2, 3)
+		print 'Distance: ' + repr(distance)
+
+def getNeighbors(trainingSet, testInstance, k):
+	distances = []
+	length = len(testInstance)-1
+	for x in range(len(trainingSet)):
+		dist = euclideanDistance(testInstance, trainingSet[x], length)
+		distances.append((trainingSet[x], dist))
+	distances.sort(key=operator.itemgetter(1))
+	neighbors = []
+	for x in range(k):
+		neighbors.append(distances[x][0])
+		return neighbors
+
+		#to test the getNeighbors function
+		trainSet = [[2, 2, 2, 'a'], [4, 4, 4, 'b']]
+		testInstance = [5, 5, 5]
+		k = 1
+		neighbors = getNeighbors(trainSet, testInstance, 1)
+		print(neighbors)
